6b22c53b134801c1cf7b628ef51cfe4f034b9f1c
JMoratelli <jurandirmoratelli@hotmail.com>
2025-03-21 15:38:04 -0400

    Update RadioRaspi.sh

--- RadioRaspi.sh
+++ RadioRaspi.sh
@@ -35,15 +35,36 @@ sudo raspi-config nonint do_configure_keyboard br
 #Configura para desativar o Waylan e utilizar interface X11
 sudo raspi-config nonint do_wayland W1
 
-#Ativa o VNC
-sudo raspi-config nonint do_vnc 0
-
 #atualiza a lista apt e atualiza os pacotes do raspiberry
 sudo apt update && sudo apt upgrade -y
 
-echo "Script realizado por Jurandir Moratelli IG @jjmoratelli ;)"
-sleep 5
-reboot
+#Reservado!
 #sudo nano /etc/xdg/lxsession/LXDE-pi/autostart
 #@chromium-browser --test-type --kiosk --incognito --noerrdialogs --no-context-menu --no-sandbox --disable-infobars --disable-translate --disable-pinch http://192.168.12.247/pesquisa/primavera-leste.php
 
+echo "Passo necessário para configuração do acesso via VNC!"
+echo "Insira a senha root para configuração do VNC e a repita novamente"
+sudo passwd root 
+
+#Digite a senha root para acesso ao modo root
+echo "Digite a senha root que acabou de criar"
+su
+
+#Ativa o VNC
+sudo raspi-config nonint do_vnc 0
+#Define opções de senha e criptografia
+sudo printf "Encryption=PreferOff\nAuthentication=VncAuth" > /root/.vnc/config.d/vncserver-x11
+read -s -p "Digite a senha do VNC: " senhaVNC
+# Definir a senha do VNC para "pleste" e ativar login legado
+sudo vncpasswd -service -legacy <<< "$senhaVNC"
+echo "Senha do VNC definida e login legado ativado."
+
+# Configurar o RealVNC para iniciar automaticamente no boot
+sudo systemctl enable vncserver-x11-serviced
+echo "RealVNC configurado para iniciar automaticamente."
+
+echo "Configuração do RealVNC concluída."
+
+echo "Script realizado por Jurandir Moratelli IG @jjmoratelli ;)"
+sleep 5
+sudo reboot
